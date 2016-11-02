eca07a00194f29631157457692648622c12522b4
cclarissa <cclarissachen@gmail.com>
2016-11-02 22:09:10 +0700

    a
    commit

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+</head>
+<body>
+
+<?php
+require_once "db.php";
+if(isset($_SESSION["username"])){
+	header("Location:content.php");
+}
+
+if(! isset($_POST["username"]) || ! isset($_POST["password"])){
+
+}else{
+	$username= $_POST['username'];
+	$password= sha1($_POST['password']);
+
+	$conn = konek_db();
+	$query = $conn->prepare("select * from user where username=? and password=?");
+	$query->bind_param("ss",$username,$password);
+	$result = $query->execute();
+
+	if($result) {
+		$res = $query->get_result();
+		if($res->num_rows == 1){
+			$_SESSION["username"] = $username;
+			header("Location: content.php");
+		}else {
+		echo "<p>USername/password salah</p>";
+	} 
+	}else{
+		echo "<p>Lagi ada masalah dengan sysmtem</p>";
+}
+}
+?> 
+</body>
+</html>
