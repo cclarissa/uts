0127e4e08ea6983d8909f3462690e47ed8bbc038
cclarissa <cclarissachen@gmail.com>
2016-11-06 17:24:13 +0700

    content
    commit

---
+++ content.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        .container { 
+        	padding: 100px;
+        	padding-left: 580px;
+            width: 20%;
+            height: 100px;
+        }
+
+        p, li { font-family : Sans-serif; }
+
+        p { color: #999; }
+
+        .nav ul {
+            list-style-type: none;
+            padding-left: 8px;
+        }
+
+        .nav li { padding: 4px; }
+
+        .input { padding-left: 50px; }
+
+        .foot { padding-left: 580px; }
+        .foot p { font-size: 12px; }
+
+    </style>
+
+    <meta charset="UTF-8">
+    <title>WELCOME BACK</title>
+</head>
+<body>
+<div class="container">
+<p>Selamat Datang</p>
+</div>
+</body>
+</html>
